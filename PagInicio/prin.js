de512b947121fae16d83cff541d8f950ff48a21b
devyare <yarenifrancisco@gmail.com>
2026-07-17 21:16:09 -0600

    Se agrego imagen flotante a pag Inicio

--- PagInicio/prin.js
+++ PagInicio/prin.js
@@ -1,3 +1,4 @@
+
 const tarjetas = document.querySelectorAll('#descripcion > div');
 let indice = 0;
 
@@ -13,3 +14,19 @@ if (tarjetas.length > 0) {
     ciclarTarjetas(); 
     setInterval(ciclarTarjetas, 3000); 
 }
+const imagen = document.querySelector('#imagen-banner img');
+let subiendo = true;
+let posicion = 0;
+
+function flotarImagen() {
+    if (subiendo) {
+        posicion += 1;
+        if (posicion >= 15) subiendo = false;
+    } else {
+        posicion -= 1;
+        if (posicion <= 0) subiendo = true;
+    }
+    imagen.style.transform = `translateY(${posicion}px)`;
+}
+
+setInterval(flotarImagen, 30);
